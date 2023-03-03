9d328da5839e15d978bcc512657eee09cdce0920
America-Miguel-Parra <isc19350307@gmail.com>
2023-03-03 10:30:39 -0600

    Diseño de la calculadora, diseño de botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,47 @@
+import './App.css'
+
 function App() {
   return(
     <div>
     <h1>Calculator</h1>
-    <table style={{width: '400px'}}>
+    <table>
+      {/*first row */}
+      
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td colSpan={4}>RESULTADO</td>
       </tr>
-
+      
+      {/*second row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">C</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">/</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">*</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">-</button> </td>
       </tr>
-
+      {/*Three row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">7</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">8</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">9</button> </td>
+        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-3">+</button> </td>
       </tr>
-
+      {/*Four row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">6</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">5</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">4</button> </td>
       </tr>
-
+      {/*five row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">3</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">2</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">1</button> </td>
+        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-3">=</button> </td>
       </tr>
-
+      {/*Six row */}
       <tr>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
-        <td style={{border: '1px solid black'}}>0</td>
+        <td colSpan={2}> <button type="button" class="btn btn-primary w-100 p-3">0</button> </td>
+        <td> <button type="button" class="btn btn-primary w-100 p-3">.</button> </td>
       </tr>
 
     </table>
