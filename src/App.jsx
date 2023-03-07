12fe3f7344727c3c9c18ab4816501983b9b6e63f
America-Miguel-Parra <isc19350307@gmail.com>
2023-03-07 10:02:05 -0600

    , diseño de botones

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <td> <button type="button" class="btn btn-primary w-100 p-3">7</button> </td>
         <td> <button type="button" class="btn btn-primary w-100 p-3">8</button> </td>
         <td> <button type="button" class="btn btn-primary w-100 p-3">9</button> </td>
-        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-3">+</button> </td>
+        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-5">+</button> </td>
       </tr>
       {/*Four row */}
       <tr>
@@ -36,7 +36,7 @@ function App() {
         <td> <button type="button" class="btn btn-primary w-100 p-3">3</button> </td>
         <td> <button type="button" class="btn btn-primary w-100 p-3">2</button> </td>
         <td> <button type="button" class="btn btn-primary w-100 p-3">1</button> </td>
-        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-3">=</button> </td>
+        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-5">=</button> </td>
       </tr>
       {/*Six row */}
       <tr>
