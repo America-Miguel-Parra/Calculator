78c699f66a3d6f61edc8065cc9babf00a06c8a32
America-Miguel-Parra <isc19350307@gmail.com>
2023-03-10 10:22:50 -0600

    agregando funcion en el boton 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import './App.css'
+import { useState } from 'react'
+
+const buttonClass = "btn btn-primary w-100 p-5" 
+const buttonsClasses = "btn btn-primary w-100 p-3" 
 
 function App() {
+
+  const [screen, setScreen] = useState('0')
+
+  const handleButtonClick = () => {
+    if(screen === '0') {
+      setScreen('9')
+    } else {
+      setScreen(`${screen}9`)
+    }
+  }
+  
   return(
     <div className='app'>
     <h1 className='shadow-sm'>Calculator</h1>
@@ -9,43 +24,45 @@ function App() {
       
       <tr>
         <td colSpan={4} style={{border: "1px solid black", textAlign: "end"}}>
-          <h2>0</h2>
+          <h2>{screen}</h2>
         </td>
       </tr>
       
       {/*second row */}
       <tr>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">C</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">/</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">*</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">-</button> </td>
+        <td> <button type="button" className={buttonsClasses}>C</button> </td>
+        <td> <button type="button" className={buttonsClasses}>/</button> </td>
+        <td> <button type="button" className={buttonsClasses}>*</button> </td>
+        <td> <button type="button" className={buttonsClasses}>-</button> </td>
       </tr>
       {/*Three row */}
       <tr>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">7</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">8</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">9</button> </td>
-        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-5">+</button> </td>
+        <td> <button type="button" className={buttonsClasses}>7</button> </td>
+        <td> <button type="button" className={buttonsClasses}>8</button> </td>
+        <td> <button type="button" className={buttonsClasses}
+        onClick={handleButtonClick}
+        >9</button></td>
+        <td rowSpan={2}> <button type="button" className={buttonClass}>+</button> </td>
       </tr>
       {/*Four row */}
       <tr>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">6</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">5</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">4</button> </td>
+        <td> <button type="button" className={buttonsClasses}>6</button> </td>
+        <td> <button type="button" className={buttonsClasses}>5</button> </td>
+        <td> <button type="button" className={buttonsClasses}>4</button> </td>
       </tr>
       {/*five row */}
       <tr>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">3</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">2</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">1</button> </td>
-        <td rowSpan={2}> <button type="button" class="btn btn-primary w-100 p-5">=</button> </td>
+        <td> <button type="button" className={buttonsClasses}>3</button> </td>
+        <td> <button type="button" className={buttonsClasses}>2</button> </td>
+        <td> <button type="button" className={buttonsClasses}>1</button> </td>
+        <td rowSpan={2}> <button type="button" className={buttonClass}>=</button> </td>
       </tr>
       {/*Six row */}
 
       <tr>
-      <td colSpan={1}> <button type="button" class="btn btn-primary w-100 p-3">DEL</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">0</button> </td>
-        <td> <button type="button" class="btn btn-primary w-100 p-3">.</button> </td>
+      <td colSpan={1}> <button type="button" className={buttonsClasses}>DEL</button> </td>
+        <td> <button type="button" className={buttonsClasses}>0</button> </td>
+        <td> <button type="button" className={buttonsClasses}>.</button> </td>
       </tr>
 
     </table>
